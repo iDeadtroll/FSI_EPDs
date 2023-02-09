33d47b206505e3972e706f9e2d97327abaf0ec8d
iDeadtroll <javielproductor@gmail.com>
2023-02-09 22:04:25 +0100

    EPDs
    EPDs del 1 al 3

---
+++ EDP1/Experimento1.py
@@ -0,0 +1,17 @@
+#Implementacion 1
+# a = 15
+# b = 4
+# print ("""La división entera:""" + str(a) + " entre " + str(b) + " es " + str(a // b) + " resto " + str(a % b))
+# print ("""La división real:""", a, "entre", b, "es", a / b)
+
+#Implementacion 2
+# a = 15
+# b = 4
+# print ("""La división entera:""" + str(a) + " entre " + str(b) + " es " + str(a // b) + " resto " + str(a % b))
+# print ("""La división real:""", str(a), "entre", str(b), "es", str(a / b))
+
+#Implementacion 3
+a = 15
+b = 4
+print ("""La división entera:""", a, " entre ", b," es ", (a // b), " resto ", (a % b))
+print ("""La división real:""", a, "entre", b, "es", a / b)
